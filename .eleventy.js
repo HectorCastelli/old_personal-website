4c184b4dddf6b41b205ff2ec45521e3564eba437
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2022-01-08 23:53:01 +0100

    feat: add html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
+const htmlminifier = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(inclusiveLangPlugin, {
@@ -6,6 +7,20 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addWatchTarget("./_input/styles/");
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlminifier.minify(content, {
+        useShortDoctype: false,
+        removeComments: true,
+        collapseWhitespace: true,
+        html5: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
